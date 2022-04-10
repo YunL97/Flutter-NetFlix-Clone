74f9c95c8de2a675c0eb06615dc8eae230c7c65b
YunL97 <leeys9932@naver.com>
2022-04-11 00:26:36 +0900

    ListView 공부

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -10,73 +10,63 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     //MaterialApp: 구글이제공하는 Material을 사용가능 but google스타일 custom 하고싶을때에도 사용가능 MeterialApp 사용안하면 코드 3배 증가
-   return MaterialApp(
-     //Scaffold
-      home: Scaffold(
+    return MaterialApp(
+        //Scaffold
+        home: Scaffold(
+            appBar: AppBar(
+              leading: Icon(Icons.star),
+              title: Text("aaa"),
+            ),
+            body: Container(
+              width: double.infinity,
+              child: ListView(
 
-        appBar: AppBar(
-          leading: Icon(Icons.star), title: Text("aaa"),
-        ),
-        body: Container(
-          // child: Container(
-            // width:double.infinity, height: 50,color: Colors.blue,
-            // margin: EdgeInsets.all(20),
-            // // padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
-            //border같은 잡다한 설정 넣을때 decoration사용용          // decoration: BoxDecoration(
-            //   border: Border.all(color: Colors.black)
-            // ),
-            // child: Text('test',
-            //     style: TextStyle(backgroundColor: Colors.red, fontWeight: FontWeight.w700)
-            // ),
-          // child: ElevatedButton(child: Text('button'),
-          //   onPressed: (){},
-          //   style: ButtonStyle(alignment: Alignment.bottomCenter),
-          height: 150 ,
-           padding: EdgeInsets.all(20),
-           child: Row(
-             children: [
-               Image.asset("du.jpg", width: 150,),
-               Container(
-                 width: 300,
-                 child: Column(
-                   crossAxisAlignment: CrossAxisAlignment.start,
-                   children: [
-                     Text("aaaaaaaaaaa",),
-                     Text("aaaaa"),
-                     Text("aaaaa"),
-                     Row(
-                       mainAxisAlignment: MainAxisAlignment.end,
-                       children: [
-                         Icon(Icons.favorite),
-                         Text('4')
-                       ],
-                     )
-                   ],
-                 ),
-               )
-             ],
-           ),
-        ),
-
-
-
-        bottomNavigationBar: BottomAppBar(
-            child: Container(
-              height: 70,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Icon(Icons.add_alert),
-                  Icon(Icons.star),
-                  Icon(Icons.star),
+                  Container(
+                    color: Colors.grey,
+                    child: Row(
+
+                      children: [
+                        Icon(Icons.star),
+                        Text("aa")
+                      ],
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.star),
+                      Text("aa")
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.star),
+                      Text("aa")
+                    ],
+                  ),
                 ],
               ),
-            ),
-        ),
-      )
+            )
+        )
     );
   }
 }
 
+//ios 클로저:? 변수에다가 담아도 되는것: 평생 바뀌지 않는것 ex)로고 상단바 하단바,    데이터가 담기는것 같은경우는 변수에 담지 않는다 성능 이슈가 날 수 있기 때문
+var a = SizedBox(
+  child: Text("bb")
+);
+
+class ShopItem extends StatelessWidget {
+  //class에 어떤 파라미터 넣을 수 있는지 정의부분
+  const ShopItem({Key? key}) : super(key: key);
+
+  //class 안에 build 라는 함수 만드는 부분
+  @override //@override : 상위 클래스 함수보다 내꺼먼저 적용하라는 뜻
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Text("aa"),
+    );
+  }
+}
